d53fe085268c52bed204135f1683ebc1e4a6a386
pugbabu <243931551@qq.com>
2018-05-18 15:49:46 +0800

    feat:新增html模版

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const Koa = require('koa')
+const app = new Koa()
+const {normal} = require('./tpl/index')
+app.use(async (ctx, next) => {
+    ctx.type = 'text/html;charset=utf-8'
+    ctx.body = normal
+})
+app.listen(4455)
