ea447d2b100db0764f7a63c918779fe6381357f2
pugbabu <243931551@qq.com>
2018-05-21 20:17:27 +0800

    dfd

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # movie
 
 > 电影预告网
@@ -17,5 +18,5 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
-测试哈哈
+测试哈哈1111
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ tasks/qiniu.js
@@ -0,0 +1,7 @@
+const movies = {   
+    video: 'http://vt1.doubanio.com/201805211601/34b895e925ca2d8fecdeb765c4fc2e06/view/movie/M/402310093.mp4',
+    doubanId: '27133303',
+    cover: 'https://img1.doubanio.com/img/trailer/medium/2522332957.jpg?',
+    poster: 'https://img1.doubanio.com/view/photo/l_ratio_poster/public/p2520331478.jpg'
+   
+}
