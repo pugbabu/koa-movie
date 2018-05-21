a89355ce210d7d12fe4a995400b9a615f7bed7b3
pugbabu <243931551@qq.com>
2018-05-21 13:30:41 +0800

    feat: 新增爬虫子进程

---
+++ crawler/index.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer')
+const url = 'https://movie.douban.com/tag/#/?sort=R&range=6,10&tags='
+
+const sleep = time => new Promise(resolve => {
+  setTimeout(resolve, time)
+})
+
+;(async () => {
+  console.log('开始爬取')
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    dumpio: false
+  })
+
+  const page = await browser.newPage()
+  await page.goto(url, {
+    waitUntil: 'networkidle2'
+  })
+
+  await sleep(3000)
+
+  await page.waitForSelector('.more')
+
+  for (let i = 0; i < 1; i++) {
+    await sleep(3000)
+    await page.click('.more')
+  }
+  console.log(111)
+  const result = await page.evaluate(() => {
+    console.log(1212112)
+    let $ = window.$
+    let items = $('.list-wp a')
+    let links = []
+    console.log(items)
+    if (items.length >= 1) {
+      items.each((index, item) => {
+        let it = $(item)
+        let doubanId = it.find('div').data('id')
+        let title = it.find('.title').text()
+        let rate = Number(it.find('.rate').text())
+        let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
+        links.push({
+          doubanId,
+          title,
+          rate,
+          poster
+        })
+      })
+    }
+    return links
+  })
+  browser.close()
+  process.send({result})
+  process.exit(0)  
+})()
